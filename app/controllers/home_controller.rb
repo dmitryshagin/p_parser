56cf5f16269ca51a1e85d24d5c6e8d07c032ca9e
Дмитрий Шагин <d.shagin@smcs.com.ua>
2016-05-24 12:05:46 +0300

    added vgsu parser

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,16 @@ class HomeController < ApplicationController
     sheet = Roo::Spreadsheet.open(t.path).sheet(0)
     records = []
     2.upto(sheet.last_row) { |i| records << parse_record(sheet.row(i)) }
-    render text: records
+    render text: check_vgsu(records)
+    # render text: records
+  end
+
+  def check_vgsu(records)
+    res = []
+    records.first(2).each do |record|
+      res << [record[2], Vgsu.parse(record[2])]
+    end
+    res
   end
 
   private
